a9d8f715ceef6838343874bef61128b2ee3d8dc5
asketsystem <asketsystem1@gmail.com>
2021-11-28 18:36:18 +0000

    Creating a new contact(POST request)

---
+++ lib/controllers/crmControllers.ts
@@ -0,0 +1,21 @@
+// /lib/controllers/crmController.ts
+
+import * as mongoose from 'mongoose';
+import { ContactSchema } from '../models/crmModel';
+import { Request, Response } from 'express';
+
+const Contact = mongoose.model('Contact', ContactSchema);
+export class ContactController{
+    
+    public addNewContact (req: Request, res: Response) {
+        let newContact = new Contact (req.body);
+
+        newContact.save((err, contact) => {
+            if(err) {
+                res.send(err);
+            }
+            res.json(contact);
+        });
+    }
+
+}
